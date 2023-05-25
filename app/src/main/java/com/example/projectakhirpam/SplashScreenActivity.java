6ba5820ca3ce2dcad18cfbc84745154d1d3bff2b
yehezkiel imannuel <yehezkielimannuel1@gmail.com>
2023-05-25 19:50:49 +0700

    Menyambungkan SplashScreen ke homepage

---
+++ app/src/main/java/com/example/projectakhirpam/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.projectakhirpam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run(){
+                try {
+                    super.run();
+                    sleep(1000);
+                } catch (Exception e){
+
+                } finally {
+                    startActivity(new Intent(SplashScreenActivity.this, HomepageActivity.class));
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
