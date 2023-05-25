d75f59f857167972851dd296539b4ad962b3ee42
Alishza Putri Rahmadina <103365920+alishzaputri@users.noreply.github.com>
2023-05-25 21:04:12 +0700

    Menambah intent pada homepage

--- app/src/main/java/com/example/projectakhirpam/HomepageActivity.java
+++ app/src/main/java/com/example/projectakhirpam/HomepageActivity.java
@@ -1,14 +1,27 @@
 package com.example.projectakhirpam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements View.OnClickListener{
+    Button _btStart;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        _btStart = findViewById(R.id.bt_start);
+
+        _btStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
